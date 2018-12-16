dc0959882be5731a44bf2e47c078fdd0d4aca0bf
Brenden Williams <brendenw7@gmail.com>
2018-12-16 15:54:19 -0500

    Site Runs
    Need to fix collision detection

---
+++ about-me-game/server/server.js
@@ -0,0 +1,13 @@
+// console.log('We here');
+
+// let express = require('express');
+
+// let app = express();
+
+// server = app.listen(3000, listening);
+
+// function listening() {
+// 	console.log('listening ....');
+// }
+
+// app.use(express.static('public'));
