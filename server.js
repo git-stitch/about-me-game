c9ebaf90bc75f7b871cde2d65dc1d0f27ea6d36d
unknown <brendenw7@gmail.com>
2018-12-16 21:45:23 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 console.log('We here');
+const PORT = process.env.PORT || 5000;
 
 let express = require('express');
 
 let app = express();
 
-server = app.listen(3000, listening);
+server = app.listen(PORT, listening);
 
 function listening() {
 	console.log('listening ....');
